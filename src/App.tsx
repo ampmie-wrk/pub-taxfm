c131bbe8278bcd202cfbf52d73cbad617392bc40
ampmie-wrk <ampmie.wrk@gmail.com>
2026-09-25 22:52:18 +0700

    Fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useRef } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 
 // ข้อมูลกองทุนและผู้จัดการกองทุน (AMC)
